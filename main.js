8158dabbdb4c2237e66ab2b12c96f105bded9529
zain135sub <109743353+zain135sub@users.noreply.github.com>
2023-01-22 18:24:08 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+status=""
+objects=[]
+song=""
+function preload(){
+    song=loadSound("siren.wav")
+    }
+
+function setup(){
+    canvas=createCanvas(380,380)
+    canvas.center()
+    video=createCapture(VIDEO)
+    video.size(380,380)
+    video.hide()
+}
+function modalloaded(){
+    console.log("modal is loaded!!")
+    status=true
+}
+function start(){
+    objectDetector=ml5.objectDetector("cocossd",modalloaded)
+    document.getElementById("status").innerHTML="status:Detecting Baby"
+}
+function draw(){
+    image(video,0,0,380,380)
+
+    if(status!=""&&result.lengths=="person"){
+        r=random(255)
+        g=random(255)
+        b=random(255)
+        objectDetector.detect(video,gotResult)
+        for(i=0;i<objects.length;i++){
+            document.getElementById("status").innerHTML=" status:Object Detected"
+            document.getElementById("number_of_objects").innerHTML="number of objects detected are:"+objects.length
+            fill(r,g,b)
+            percent=floor(objects[i].confidence*100)
+            text(objects[i].label+""+percent+"%",objects[i].x+15,objects[i].y+15)
+            noFill()
+            stroke(r,g,b)
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height)
+        }
+    }
+    else if(status!=""&&result.lengths=="person"){
+        document.getElementById("number_of_objects").innerHTML="baby detected"
+        song.setVolume(0)
+    }else{
+        document.getElementById("number_of_objects").innerHTML="Baby not detected"
+        song.play()
+        
+    }
+    
+}
+function gotResult(error,results){
+    if(error){
+        console.log(error)
+    }
+    else{
+        console.log(results)
+        objects=results
+    }
+}
